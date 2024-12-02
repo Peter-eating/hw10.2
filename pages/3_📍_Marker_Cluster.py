b5f5e649f4832ec26861357e61d0d6b1c9c410b7
Peter-eating <yitinglu071@gmail.com>
2024-12-02 14:39:57 +0800

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -13,16 +13,16 @@ st.sidebar.info(markdown)
 logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
-st.title("Marker Cluster")
+st.title("KINMEN Air-Raid Shelter")
 
 with st.expander("See source code"):
     with st.echo():
 
         m = leafmap.Map(center=[40, -100], zoom=4)
         cities = "https://github.com/Peter-eating/hw10.2/raw/refs/heads/main/KINMEN%20Air-Raid%20Shelter%20(1).csv"
-        regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
+        regions = "https://github.com/Peter-eating/hw10.2/raw/refs/heads/main/KINMEN.shp"
 
-        m.add_geojson(regions, layer_name="US Regions")
+        m.add_geojson(regions, layer_name="鄉鎮區界")
         m.add_points_from_xy(
             cities,
             x="Longitude",
