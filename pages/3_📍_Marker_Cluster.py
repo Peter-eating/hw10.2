b36ad60d9422f0c0fe2f954768effcf727ec8a53
Peter-eating <yitinglu071@gmail.com>
2024-12-02 14:28:31 +0800

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -19,14 +19,14 @@ with st.expander("See source code"):
     with st.echo():
 
         m = leafmap.Map(center=[40, -100], zoom=4)
-        cities = "https://github.com/8048-kh/test/raw/refs/heads/main/Aboriginal%20Tribes%20area.csv"
+        cities = "https://github.com/Peter-eating/hw10.2/raw/refs/heads/main/KINMEN%20Air-Raid%20Shelter%20(1).csv"
         regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
 
         m.add_geojson(regions, layer_name="US Regions")
         m.add_points_from_xy(
             cities,
-            x="longitude",
-            y="latitude",
+            x="Longitude",
+            y="Latitude",
             color_column="region",
             icon_names=["gear", "map", "leaf", "globe"],
             spin=True,
